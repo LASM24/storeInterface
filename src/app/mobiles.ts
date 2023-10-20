7a00878f87b3d007ad0f3705ad17a76db80bd33d
LASM24 <luisalbertosanabriamendez@gmail.com>
2023-10-19 20:35:37 -0500

    update images

--- src/app/mobiles.ts
+++ src/app/mobiles.ts
@@ -33,49 +33,49 @@ export interface Product {
       name: 'Phone X Mini',
       price: 699,
       description: 'Un gran teléfono con una de las mejores cámaras',
-      photo: '../assets/img/minii.png'
+      photo: '../assets/img/minii.png',
     },
     {
       id: 5,
       name: 'Pixel 4a',
       price: 500,
       description: 'hola :D',
-      photo: '../assets/img/pixela4.jpg'
+      photo: '../assets/img/pixel4.png'
     },
     {
       id: 6,
       name: 'Pixel 5',
       price: 700,
       description: 'Flafship de Google a un precio asequible',
-      photo: '../assets/img/pixela5.jpg'
+      photo: '../assets/img/pixell5.png'
     },
     {
       id: 7,
       name: 'Pixel 5 XL',
       price: 800,
       description: 'Flafship de Google a un precio asequible' ,
-      photo: '../assets/img/pixela5XL.jpg'
+      photo: '../assets/img/pixel5XL.png'
     },
     {
       id : 8,
       name: 'Pixel 6',
       price: 900,
       description: 'La mejor expereincia de Android',
-      photo: '../assets/img/pixel6.jpg'
+      photo: '../assets/img/pixell6.png'
     },
     {
       id: 9,
       name: 'Pixel 6 XL',
       price: 1000,
       description: 'La mejor expereincia de Android en un teléfono grande',
-      photo: '../assets/img/pixel6XL.jpg'
+      photo: '../assets/img/6XL.png'
     },
     {
       id: 10,
       name: 'OnePlus 9 Pro',
       price: 899,
       description: 'El teléfono insignia de OnePlus con excelentes características',
-      photo: '../assets/img/oneplus.jpg'
+      photo: '../assets/img/one.png'
     },
     {
       id: 11,
